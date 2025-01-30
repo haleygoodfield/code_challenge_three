5b71fafa19ea6fe21460771e6bc5f4d9d7c74817
Haley Goodfield <haleygoodfield@usf.edu>
2025-01-29 20:22:21 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,15 @@
+// Task 1: Product Price Management Scenario: Online Store You are managing a product pricing system
+
+// Declare an array prices using let and initialize it with at least five numerical values.
+let prices = [25, 50, 75, 100, 125];
+
+// Add a new price to the array.
+prices.push(15); // Add 15
+
+// Remove the first price from the array.
+prices.slice(1); // Remove 15
+
+// Log the updated price list to the console.
+console.log("Updated Price List: $ ", prices);
+
+
